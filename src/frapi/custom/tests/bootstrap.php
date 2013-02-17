4fa779955ec1d20c1ef9d06b4a859cad35276603
Dominik Jungowski <Dominik@Jungowski.de>
2013-02-17 17:59:17 +0100

    Nyan Cat fuer frapi unit tests

--- src/frapi/custom/tests/bootstrap.php
+++ src/frapi/custom/tests/bootstrap.php
@@ -11,9 +11,13 @@ $frapiPath = array_slice($path, 0, count($path) - 1);
 $path = implode(DIRECTORY_SEPARATOR, $path);
 
 define('BASEPATH', $path);
-define('CUSTOM_MODEL', $path . DIRECTORY_SEPARATOR . '/Model');
 
 // Frapi dependencies
 $frapiPath = implode(DIRECTORY_SEPARATOR, $frapiPath) . '/library/Frapi';
 $_SERVER['HTTP_HOST'] = 'api.score11.de';
 require_once $frapiPath . '/AllFiles.php';
+
+// Nyan Cat phpunit Printer
+require_once 'vendor/whatthejeff/fab/src/Fab/Factory.php';
+require_once 'vendor/whatthejeff/fab/src/Fab/Fab.php';
+require_once 'vendor/whatthejeff/fab/src/Fab/SuperFab.php';
